0e1232495f8ad4a18c3722441bf3ce41a3be1cc0
arbaztalk2me <geekyarbaz@gmail.com>
2024-01-04 17:31:54 +0530

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'componentExample';
+  
+   posts=[
+    {
+      "title":"Tree",
+      "subtitle":"@arbaz",
+      "url":"https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "content":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 150"
+    },
+    {
+      "title":"car",
+      "subtitle":"@jack",
+      "url":"https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "content":"t is a long established fact that a reader will be distracted by the readable content of a "
+    },
+    {
+      "title":"Travel",
+      "subtitle":"soniya",
+      "url":"https://images.pexels.com/photos/346885/pexels-photo-346885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "content":" popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,"
+    },
+    
+  ];
 }
